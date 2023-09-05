a5b818f6994c99aad3f32b12f59f71d955cba5ad
fesmofet <borees2@gmail.com>
2023-09-05 16:22:05 +0300

    5858 dynamic google agents

--- src/api/botAgent.js
+++ src/api/botAgent.js
@@ -1,7 +1,24 @@
 const db = require('../mongo');
 
+const GOOGLE_USERAGENT_REGEX1 = /Mozilla\/5\.0 \(Linux; Android 6\.0\.1; Nexus 5X Build\/MMB29P\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.\d+\.\d+\.\d+ Mobile Safari\/537\.36 \(compatible; Googlebot\/2\.1; \+http:\/\/www\.google\.com\/bot\.html\)/;
+const GOOGLE_USERAGENT_REGEX2 = /Mozilla\/5\.0 AppleWebKit\/537\.36 \(KHTML, like Gecko; compatible; Googlebot\/2\.1; \+http:\/\/www\.google\.com\/bot\.html\) Chrome\/\d+\.\d+\.\d+\.\d+ Safari\/537\.36/;
+const GOOGLE_USERAGENT_REGEX3 = /Mozilla\/5\.0 \(Linux; Android 6\.0\.1; Nexus 5X Build\/MMB29P\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.\d+\.\d+\.\d+ Mobile Safari\/537\.36 \(compatible; Google-InspectionTool\/1\.0\)/;
+const GOOGLE_USERAGENT_REGEX4 = /Mozilla\/5\.0 \(Linux; Android 6\.0\.1; Nexus 5X Build\/MMB29P\) AppleWebKit\/537\.36 \(KHTML, like Gecko\) Chrome\/\d+\.\d+\.\d+\.\d+ Mobile Safari\/537\.36 \(compatible; AdsBot-Google-Mobile; \+http:\/\/www\.google\.com\/mobile\/adsbot\.html\)/;
+
+const USER_AGENTS_DYNAMIC_REGEX = [
+  GOOGLE_USERAGENT_REGEX1,
+  GOOGLE_USERAGENT_REGEX2,
+  GOOGLE_USERAGENT_REGEX3,
+  GOOGLE_USERAGENT_REGEX4,
+];
+
+const testStringAgainstAgentRegex = (str) => (
+  USER_AGENTS_DYNAMIC_REGEX.some((regex) => regex.test(str)));
+
 const userAgentExists = async ({ userAgent = '' }) => {
   if (!userAgent) return false;
+  const isDynamicUserAgent = testStringAgainstAgentRegex(userAgent);
+  if (isDynamicUserAgent) return true;
   return db.botUserAgentRepository.userAgentExists({ userAgent });
 };
 
